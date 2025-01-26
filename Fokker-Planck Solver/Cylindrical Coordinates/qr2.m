8d9c452a0a2853b76fc47df8dbac54ded2941dcf
Dylan <djacobs2@swarthmore.edu>
2025-01-26 09:17:49 -0700

    January updates

--- Fokker-Planck Solver/Cylindrical Coordinates/qr2.m
+++
@@ -1,4 +0,0 @@
-function [Q, R] = qr2(V0, rvals)
-    [Q, R] = qr(sqrt(rvals) .* V0, 0);
-    Q = (1./sqrt(rvals)).*Q;
-end
--- Fokker-Planck Solver/Cylindrical Coordinates/svd2.m
+++
@@ -1,4 +0,0 @@
-function [U, S, V] = svd2(A, rvals)
-    [U, S, V] = svd(sqrt(rvals) .* A, 0);
-    U = (1./sqrt(rvals)) .* U;
-end
